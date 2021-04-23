56ca4a03b2b1a84b80ec71b2a21463b27273300e
ssding <dingss@gjmetal.com>
2021-04-23 17:57:23 +0800

    no message

---
+++ src/MyWebApi/FileOperate/FileOperate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Xml;
+
+namespace FileOperate
+{
+    public class FileOperate
+    {
+        public static void ReadXml(string filePath)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(filePath))
+                {
+                    Console.WriteLine("文件路径不能为空");
+                    return;
+                }
+
+                if (!File.Exists(filePath))
+                {
+                    Console.WriteLine("文件未找到");
+                }
+
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(filePath);
+                XmlNodeList nodeList = xmlDoc.SelectSingleNode();
+                foreach (XmlNode item in xmlDoc.ChildNodes)
+                {
+                    Console.WriteLine("OuterXml：{0}，InnerText：{1}", item.OuterXml, item.InnerText);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("异常：{0}", ex.Message));
+            }
+        }
+    }
+}
---
+++ src/MyWebApi/FileOperate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"H:\mine2\MyApi\src\MyWebApi\FileOperate\Doc\test.config";
+            FileOperate.ReadXml(filePath);
+        }
+    }
+}
